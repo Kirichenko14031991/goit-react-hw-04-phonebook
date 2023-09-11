29a686af700779bf89588032117c109d935132cf
Kirichenko14031991 <124730976+Kirichenko14031991@users.noreply.github.com>
2023-09-11 19:47:00 +0300

    update after check

--- src/components/contactForm/contactForm.jsx
+++ src/components/contactForm/contactForm.jsx
@@ -1,74 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FormContainer, FormLabel, FormInput, FormButton } from './contactForm.styled';
 
-class ContactForm extends Component {
-  state = {
+const ContactForm = ({ onFormSubmit, contacts }) => {
+  const [formData, setFormData] = useState({
     name: '',
     number: ''
-  }
+  });
 
-  handleChange = e => {
+  const { name, number } = formData;
+
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
-  }
+    setFormData({ ...formData, [name]: value });
+  };
 
-  handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const { name, number } = this.state;
-    this.reset();
 
-    const isNameExist = this.props.contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase());
+    const isNameExist = contacts.some(contact =>
+      contact.name.toLowerCase() === name.toLowerCase()
+    );
+
     if (isNameExist) {
       alert(`${name} is already in contacts.`);
       return;
     }
-    
-    this.props.onFormSubmit({ name, number });
+
+    onFormSubmit(formData);
+    reset();
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  }
+  const reset = () => {
+    setFormData({ name: '', number: '' });
+  };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <FormContainer onSubmit={this.handleSubmit}>
-        <FormLabel>
-          Name
-          <FormInput
-            value={name}
-            onChange={this.handleChange}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </FormLabel>
+  return (
+    <FormContainer onSubmit={handleSubmit}>
+      <FormLabel>
+        Name
+        <FormInput
+          value={name}
+          onChange={handleChange}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </FormLabel>
 
-        <FormLabel>
-          Number
-          <FormInput
-            value={number}
-            onChange={this.handleChange}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </FormLabel>
-        <FormButton type='submit' >Add contact</FormButton>
-      </FormContainer>
-    );
-  }
-}
+      <FormLabel>
+        Number
+        <FormInput
+          value={number}
+          onChange={handleChange}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </FormLabel>
+      <FormButton type="submit">Add contact</FormButton>
+    </FormContainer>
+  );
+};
 
 ContactForm.propTypes = {
   onFormSubmit: PropTypes.func.isRequired,
-  contacts: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string.isRequired))
-}
+  contacts: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string.isRequired)),
+};
 
 export default ContactForm;
